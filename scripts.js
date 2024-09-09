1ea8eb8c5654581f245ef9b9b52260c8f02bdec3
Abhishek <abhishek.pise76@gmail.com>
2024-09-09 19:47:49 +0530

    Added animations and dynamic content

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('dynamic-button');
+    button.addEventListener('click', function() {
+        alert('Button clicked! This is a dynamic interaction!');
+    });
+});
+
