26ec98d6f62a924e6761ee2830b0e226d66d1837
Abhishek <abhishek.pise76@gmail.com>
2024-09-14 11:43:44 +0530

    Updated index.html and removed unnecessary files

--- scripts.js
+++
@@ -1,8 +0,0 @@
-// scripts.js
-document.addEventListener('DOMContentLoaded', function() {
-    const button = document.getElementById('dynamic-button');
-    button.addEventListener('click', function() {
-        alert('Button clicked! This is a dynamic interaction!');
-    });
-});
-
